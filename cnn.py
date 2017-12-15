876965663116e4fd7115fb2e26c95bf61cb516f4
ShuyangDu <shuyangdu139@gmail.com>
2017-12-14 21:47:39 -0800

    update data frame and main notebook

--- cnn.py
+++ cnn.py
@@ -1,105 +1,10 @@
-import os
-
-import numpy as np
 import tensorflow as tf
 
-from data_frame import DataFrame
 from tf_base_model import TFBaseModel
 from tf_utils import (
     time_distributed_dense_layer, temporal_convolution_layer,
     sequence_mean, sequence_smape, shape
 )
-
-
-class DataReader(object):
-
-    def __init__(self, data_dir):
-        data_cols = [
-            'data',
-            'is_nan',
-            'page_id',
-            'project',
-            'access',
-            'agent',
-            'test_data',
-            'test_is_nan'
-        ]
-        data = [np.load(os.path.join(data_dir, '{}.npy'.format(i))) for i in data_cols]
-
-        self.test_df = DataFrame(columns=data_cols, data=data)
-        self.train_df, self.val_df = self.test_df.train_test_split(train_size=0.95)
-
-        print 'train size', len(self.train_df)
-        print 'val size', len(self.val_df)
-        print 'test size', len(self.test_df)
-
-    def train_batch_generator(self, batch_size):
-        return self.batch_generator(
-            batch_size=batch_size,
-            df=self.train_df,
-            shuffle=True,
-            num_epochs=10000,
-            is_test=False
-        )
-
-    def val_batch_generator(self, batch_size):
-        return self.batch_generator(
-            batch_size=batch_size,
-            df=self.val_df,
-            shuffle=True,
-            num_epochs=10000,
-            is_test=False
-        )
-
-    def test_batch_generator(self, batch_size):
-        return self.batch_generator(
-            batch_size=batch_size,
-            df=self.test_df,
-            shuffle=True,
-            num_epochs=1,
-            is_test=True
-        )
-
-    def batch_generator(self, batch_size, df, shuffle=True, num_epochs=10000, is_test=False):
-        batch_gen = df.batch_generator(
-            batch_size=batch_size,
-            shuffle=shuffle,
-            num_epochs=num_epochs,
-            allow_smaller_final_batch=is_test
-        )
-        data_col = 'test_data' if is_test else 'data'
-        is_nan_col = 'test_is_nan' if is_test else 'is_nan'
-        for batch in batch_gen:
-            num_decode_steps = 64
-            full_seq_len = batch[data_col].shape[1]
-            max_encode_length = full_seq_len - num_decode_steps if not is_test else full_seq_len
-
-            x_encode = np.zeros([len(batch), max_encode_length])
-            y_decode = np.zeros([len(batch), num_decode_steps])
-            is_nan_encode = np.zeros([len(batch), max_encode_length])
-            is_nan_decode = np.zeros([len(batch), num_decode_steps])
-            encode_len = np.zeros([len(batch)])
-            decode_len = np.zeros([len(batch)])
-
-            for i, (seq, nan_seq) in enumerate(zip(batch[data_col], batch[is_nan_col])):
-                rand_len = np.random.randint(max_encode_length - 365 + 1, max_encode_length + 1)
-                x_encode_len = max_encode_length if is_test else rand_len
-                x_encode[i, :x_encode_len] = seq[:x_encode_len]
-                is_nan_encode[i, :x_encode_len] = nan_seq[:x_encode_len]
-                encode_len[i] = x_encode_len
-                decode_len[i] = num_decode_steps
-                if not is_test:
-                    y_decode[i, :] = seq[x_encode_len: x_encode_len + num_decode_steps]
-                    is_nan_decode[i, :] = nan_seq[x_encode_len: x_encode_len + num_decode_steps]
-
-            batch['x_encode'] = x_encode
-            batch['encode_len'] = encode_len
-            batch['y_decode'] = y_decode
-            batch['decode_len'] = decode_len
-            batch['is_nan_encode'] = is_nan_encode
-            batch['is_nan_decode'] = is_nan_decode
-
-            yield batch
 
 
 class CNN(TFBaseModel):
@@ -426,37 +331,3 @@ class CNN(TFBaseModel):
 
         return self.loss
 
-
-# if __name__ == '__main__':
-#     base_dir = './'
-#
-#     dr = DataReader(data_dir=os.path.join(base_dir, 'data/processed/'))
-#
-#     nn = CNN(
-#         reader=dr,
-#         log_dir=os.path.join(base_dir, 'logs'),
-#         checkpoint_dir=os.path.join(base_dir, 'checkpoints'),
-#         prediction_dir=os.path.join(base_dir, 'predictions'),
-#         optimizer='adam',
-#         learning_rate=.001,
-#         batch_size=128,
-#         num_training_steps=200000,
-#         early_stopping_steps=5000,
-#         warm_start_init_step=0,
-#         regularization_constant=0.0,
-#         keep_prob=1.0,
-#         enable_parameter_averaging=False,
-#         num_restarts=2,
-#         min_steps_to_checkpoint=500,
-#         log_interval=10,
-#         num_validation_batches=1,
-#         grad_clip=20,
-#         residual_channels=32,
-#         skip_channels=32,
-#         dilations=[2**i for i in range(8)]*3,
-#         filter_widths=[2 for i in range(8)]*3,
-#         num_decode_steps=64,
-#     )
-#     nn.fit()
-#     nn.restore()
-#     nn.predict()
--- data_frame.py
+++ data_frame.py
@@ -1,4 +1,5 @@
 import copy
+import os
 
 import numpy as np
 import pandas as pd
@@ -84,3 +85,94 @@ class DataFrame(object):
             self.columns.append(key)
             self.data.append(value)
         self.dict[key] = value
+
+
+class DataReader(object):
+
+    def __init__(self, data_dir):
+        data_cols = [
+            'data',
+            'is_nan',
+            'page_id',
+            'project',
+            'access',
+            'agent',
+            'test_data',
+            'test_is_nan'
+        ]
+        data = [np.load(os.path.join(data_dir, '{}.npy'.format(i))) for i in data_cols]
+
+        self.test_df = DataFrame(columns=data_cols, data=data)
+        self.train_df, self.val_df = self.test_df.train_test_split(train_size=0.95)
+
+        print 'train size', len(self.train_df)
+        print 'val size', len(self.val_df)
+        print 'test size', len(self.test_df)
+
+    def train_batch_generator(self, batch_size):
+        return self.batch_generator(
+            batch_size=batch_size,
+            df=self.train_df,
+            shuffle=True,
+            num_epochs=10000,
+            is_test=False
+        )
+
+    def val_batch_generator(self, batch_size):
+        return self.batch_generator(
+            batch_size=batch_size,
+            df=self.val_df,
+            shuffle=True,
+            num_epochs=10000,
+            is_test=False
+        )
+
+    def test_batch_generator(self, batch_size):
+        return self.batch_generator(
+            batch_size=batch_size,
+            df=self.test_df,
+            shuffle=True,
+            num_epochs=1,
+            is_test=True
+        )
+
+    def batch_generator(self, batch_size, df, shuffle=True, num_epochs=10000, is_test=False):
+        batch_gen = df.batch_generator(
+            batch_size=batch_size,
+            shuffle=shuffle,
+            num_epochs=num_epochs,
+            allow_smaller_final_batch=is_test
+        )
+        data_col = 'test_data' if is_test else 'data'
+        is_nan_col = 'test_is_nan' if is_test else 'is_nan'
+        for batch in batch_gen:
+            num_decode_steps = 64
+            full_seq_len = batch[data_col].shape[1]
+            max_encode_length = full_seq_len - num_decode_steps if not is_test else full_seq_len
+
+            x_encode = np.zeros([len(batch), max_encode_length])
+            y_decode = np.zeros([len(batch), num_decode_steps])
+            is_nan_encode = np.zeros([len(batch), max_encode_length])
+            is_nan_decode = np.zeros([len(batch), num_decode_steps])
+            encode_len = np.zeros([len(batch)])
+            decode_len = np.zeros([len(batch)])
+
+            for i, (seq, nan_seq) in enumerate(zip(batch[data_col], batch[is_nan_col])):
+                rand_len = np.random.randint(max_encode_length - 365 + 1, max_encode_length + 1)
+                x_encode_len = max_encode_length if is_test else rand_len
+                x_encode[i, :x_encode_len] = seq[:x_encode_len]
+                is_nan_encode[i, :x_encode_len] = nan_seq[:x_encode_len]
+                encode_len[i] = x_encode_len
+                decode_len[i] = num_decode_steps
+                if not is_test:
+                    y_decode[i, :] = seq[x_encode_len: x_encode_len + num_decode_steps]
+                    is_nan_decode[i, :] = nan_seq[x_encode_len: x_encode_len + num_decode_steps]
+
+            batch['x_encode'] = x_encode
+            batch['encode_len'] = encode_len
+            batch['y_decode'] = y_decode
+            batch['decode_len'] = decode_len
+            batch['is_nan_encode'] = is_nan_encode
+            batch['is_nan_decode'] = is_nan_decode
+
+            yield batch
